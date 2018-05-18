2bf6c1433a9b2377e5adfeacc9074a474b0e0140
soylisK <soylis.k@gmail.com>
2018-05-18 20:11:20 +0300

    upload info txt files..

---
+++ src/read_img.py
@@ -0,0 +1,45 @@
+# script for reading each .cmp file from directories
+# and then create images
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_cmp_file(cmp_filename):
+    with open(cmp_filename, 'rb') as fid:
+        cmp_data = np.fromfile(fid, dtype=np.float32, count=-1)
+
+    cmp_data = cmp_data.reshape((-1, nfilt))  # where nfilt = 64
+    # check if read is done right
+    fbank = 10  # put a number between 0 and 63
+    plt.plot(cmp_data[:, fbank])
+    plt.show()
+
+
+# ------------------------------------------------------------------------------
+# convert .cmp file to image
+context_width = 17
+
+    def convert_to_images(self, params):
+
+        context_width = self.context_width
+        n_frames, param_dim = params.shape
+        border = context_width // 2
+
+        params_with_borders = np.zeros(
+            (2 * border + n_frames, param_dim), dtype=np.float32)
+        params_with_borders[border:-border, :] = params
+        params_with_borders[:border, :] = params[0, :]  # broadcast
+        params_with_borders[-border:, :] = params[-1, :]  # broadcast
+
+        params_with_borders_transposed = np.transpose(params_with_borders)
+
+        params_as_images = np.zeros(
+            (n_frames, param_dim, context_width, 1), dtype=np.float32)
+
+        for i in range(n_frames):
+            params_as_images[i, :, :,
+                             0] = params_with_borders_transposed[:, i:i + context_width]
+
+        return params_as_images
