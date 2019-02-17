e35b11c07a4def95fb3e460d81f572de3dc3330a
soylisK <soylis.k@gmail.com>
2019-02-18 01:13:17 +0200

    upd..

--- src/train.py
+++ src/train.py
@@ -6,22 +6,28 @@ from lib.model_io import get_model_id
 
 model_id = get_model_id()
 
-
-
-
+n_files=150 # how many files will read
 # Create the network
 network = CNN(model_id)
-network.input()
 
-# Define the train computation graph
-network.define_train_operations()
+for i in range():
+# loop until all data are read
+    network.input(n_files)
 
-with tf.device("/gpu0:")
-# Train the network
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) #session with log about gpu exec
-try:
-    network.train(sess)
-except KeyboardInterrupt:
-    print()
-finally:
-    sess.close()
+    with tf.device("/gpu0:"):
+        # restore()
+
+        # Define the train computation graph
+        network.define_train_operations()
+
+
+        # Train the network
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) #session with log about gpu exec
+        try:
+            network.train(sess)
+            # save train
+            # save()
+        except KeyboardInterrupt:
+            print()
+        finally:
+            sess.close()
