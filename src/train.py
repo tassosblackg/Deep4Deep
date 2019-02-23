a65926ace38e982c0e51ae2d66fe74db256b44cd
soylisK <soylis.k@gmail.com>
2019-02-23 12:16:21 +0200

    fix stepping..

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from model import CNN
 from lib.model_io import get_model_id
+import read_img as rim
 
 # change this according to your path
 path_to_train_set = "/home/tassos/Desktop/DATA_ASR/ASVspoof2017_V2_train_fbank"
@@ -11,32 +12,37 @@ path_to_valid_set = "/home/tassos/Desktop/DATA_ASR/ASVspoof2017_V2_train_dev"
 model_id = get_model_id()
 
 n_tfiles=150 # how many train files will read
-n_vfiles=round(0,567*n_tfiles)
-
+n_vfiles=round(0.567*n_tfiles)
+print("a= \n")
+print(n_vfiles)
 # cheat count files number
 total_inp_files = len(os.listdir(path_to_train_set))
 
 # Create the network
 network = CNN(model_id)
-
-for i in range(total_inp_files):
+iter=0
+for i in range(1,total_inp_files,n_tfiles):
 # loop until all data are read
     network.input(n_tfiles,n_vfiles)
-
-    with tf.device("/gpu0:"):
+    # print(i)
+    # rim.read_Data("ASVspoof2017_V2_train_fbank","train_info.txt",n_tfiles)
+    with tf.device('/gpu:0'):
         # restore()
-
-        # Define the train computation graph
-        network.define_train_operations()
+        if(iter==0):
+            # Define the train computation graph
+            network.define_train_operations()
 
 
         # Train the network
-        sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) #session with log about gpu exec
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=True,allow_soft_placement = True)) #session with log about gpu exec
+        #sess= tf.Session()
         try:
-            network.train(sess,i)
-            # save train
+            network.train(sess,iter)
+            iter+=1
             # save()
         except KeyboardInterrupt:
             print()
+
         finally:
             sess.close()
+            os.remove("read_status.txt")
