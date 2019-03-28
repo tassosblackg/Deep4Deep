9e884d3ad6cb63504d949ae08fd1c34de6736032
soylisK <soylis.k@gmail.com>
2019-03-29 01:56:41 +0200

    run training..

--- src/cnn_model.py
+++ src/cnn_model.py
@@ -29,29 +29,104 @@ class CNN(object):
         self.height = 64
         self.width = 17
         self.chan = 1           # channel of image 1 or 3 if rgb
+        self.n_input = self.height * self.width*self.chan # 64x17x1
         self.n_classes = 2      # genuine or spoof --number of classes
+        self.dropout = 0.75     # dropout probality
         self.batch_size = 256   # 64 || 128 || 256
         self.train_size = 0
         self.valid_size = 0
         self.eval_size = 0
 
-        # model variables --placeholders
-        # self.X_train = None
-        # self.Y_train = None
-        # self.X_valid = None
-        # self.Y_valid = None
-        # self.train_loss = 0
-        # self.keep_prob = None
-        # self.valid_loss = None
 
     def model_architecture(self, X, keep_prob,reuse=True,is_training=True):
         with tf.variable_scope("model_architecture", reuse=reuse):
+            # Reshape input picture
+            X = tf.reshape(X, shape=[-1, self.height, self.width, 1])
+            #               --{1st BLOCK}--
+            # 1st layer
+            shape = [3,3,1,4] # first layer
+            w1 = mf.weight_dict(shape,'w1')
+            b1 = mf.bias_dict([shape[3]],'b1')
+            conv_l1 = mf.conv2d(X,w1,b1,'conv_l1')
+            conv_l1 = mf.batch_n(conv_l1,'batch_norm_l1')
+            # 2nd layer
+            shape = [3,3,4,4]
+            w2 = mf.weight_dict(shape,'w2')
+            b2 = mf.bias_dict([shape[3]],'b2')
+            conv_l2 = mf.conv2d(conv_l1,w2,b2,'conv_l2')
+            conv_l2 = mf.batch_n(conv_l2,'batch_norm_l2')
 
+            max_pool_1 = mf.max_pool(conv_l2,1,1,'max_pool_bl2')
+            #               --{2nd BLOCK}--
+            # 3d layer
+            shape = [3,3,4,8]
+            w3 = mf.weight_dict(shape,'w3')
+            b3 = mf.bias_dict([shape[3]],'b3')
+            conv_l3 = mf.conv2d(max_pool_1,w3,b3,'conv_l3')
+            conv_l3 = mf.batch_n(conv_l3,'batch_norm_l3')
+            # 4th layer
+            shape = [3,3,8,8]
+            w4 = mf.weight_dict(shape,'w4')
+            b4 = mf.bias_dict([shape[3]],'b4')
+            conv_l4 = mf.conv2d(conv_l3,w4,b4,'conv_l4')
+            conv_l4 = mf.batch_n(conv_l4,'batch_norm_l4')
 
+            max_pool_2 = mf.max_pool(conv_l4,1,1,'max_pool_bl2')
+            #               --{3d BLOCK}--
+            # 5th layer
+            shape = [3,3,8,16]
+            w5 = mf.weight_dict(shape,'w5')
+            b5 = mf.bias_dict([shape[3]],'b5')
+            conv_l5 = mf.conv2d(max_pool_2,w5,b5,'conv_l5')
+            conv_l5 = mf.batch_n(conv_l5,'batch_norm_l5')
+            # 6th layer
+            shape = [3,3,16,16]
+            w6 = mf.weight_dict(shape,'w6')
+            b6 = mf.bias_dict([shape[3]],'b6')
+            conv_l6 = mf.conv2d(conv_l5,w6,b6,'conv_l6')
+            conv_l6 = mf.batch_n(conv_l6,'batch_norm_l6')
 
-            pass
+            max_pool_3 = mf.max_pool(conv_l6,1,1,'max_pool_3')
+            #               --{4th BLOCK}
+            # 7th layer
+            shape = [3,3,16,32]
+            w7 = mf.weight_dict(shape,'w7')
+            b7 = mf.bias_dict([shape[3]],'b7')
+            conv_l7 = mf.conv2d(max_pool_3,w7,b7,'conv_l7')
+            conv_l7 = mf.batch_n(conv_l7,'batch_norm_l7')
+            # 8th layer
+            shape = [3,3,32,32]
+            w8 = mf.weight_dict(shape,'w8')
+            b8 = mf.bias_dict([shape[3]],'b8')
+            conv_l8 = mf.conv2d(conv_l7,w8,b8,'conv_l8')
+            conv_l8 = mf.batch_n(conv_l8,'batch_norm_l8')
 
-        # return out_layer
+            max_pool_4 = mf.max_pool(conv_l8,2,2,'max_pool_4')
+            #               --{5th BLOCK}
+            # 9th layer
+            shape =[3,3,32,64]
+            w9 = mf.weight_dict(shape,'w9')
+            b9 = mf.bias_dict([shape[3]],'b9')
+            conv_l9 = mf.conv2d(max_pool_4,w9,b9,'conv_l9')
+            conv_l9 = mf.batch_n(conv_l9,'batch_norm_l9')
+            # 10th layer
+            shape = [3,3,64,64]
+            w10 = mf.weight_dict(shape,'w10')
+            b10 = mf.bias_dict([shape[3]],'b10')
+            conv_l10 = mf.conv2d(conv_l9,w10,b10,'conv_l10')
+            conv_l10 = mf.batch_n(conv_l10,'batch_norm_l10')
+
+            max_pool_5 = mf.max_pool(conv_l10,2,2,'max_pool_5')
+
+            #           --{FULLY CONNECTED LAYERS}--
+            flatt_out = mf.flatten_l(max_pool_5,'flatten_out_layer')
+            fc1 = mf.fully_con(flatt_out,256,'fc1')
+            # fc1 = tf.nn.dropout(fc1,self.dropouttop)
+            fc2 = mf.fully_con(fc1,512,'fc2')
+
+            logits = mf.outp_layer(fc2,self.n_classes,'Last_layer')
+            print('Logits_shape='+str(logits.shape))
+        return logits
 
     # last layer of network is a softmax output
     def inference(self,X,keep_prob):
@@ -62,14 +137,14 @@ class CNN(object):
         # set placeholders
         self.keep_prob = tf.placeholder(dtype=tf.float32,name='keep_prob')
 
-        self.X_train   = tf.placeholder(dtype=tf.float32, shape=(None,self.height,self.width,self.chan),name='X_train')
+        self.X_train   = tf.placeholder(dtype=tf.float32, shape=(None,self.n_input),name='X_train')
 
-        self.Y_train   = tf.placeholder(dtype=tf.int32,shape=(None, ), name='Y_train')
+        self.Y_train   = tf.placeholder(dtype=tf.int32,shape=(None, self.n_classes), name='Y_train')
 
         # network prediction
         Y_train_predict = self.model_architecture(self.X_train,self.keep_prob,reuse=False)
         # calculate training loss between real label and predicted
-        self.train_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=Y_train_predict, labels=self.Y_train,name='train_loss'))
+        self.train_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=Y_train_predict, labels=self.Y_train,name='train_loss'))
 
         # define learning rate decay method
         global_step = tf.Variable(0, trainable=False, name='global_step')
@@ -84,13 +159,13 @@ class CNN(object):
         self.update_ops = optimizer.minimize(self.train_loss, var_list=trainable, global_step=global_step)
 
         # --- Validation computations
-        self.X_valid = tf.placeholder(dtype=tf.float32, shape=(None, self.height, self.width, self.chan))  # Define this
-        self.Y_valid = tf.placeholder(dtype=tf.int32, shape=(None, ))  # Define this
+        self.X_valid = tf.placeholder(dtype=tf.float32, shape=(None, self.n_input))  # Define this
+        self.Y_valid = tf.placeholder(dtype=tf.int32, shape=(None,self.n_classes))  # Define this
 
         Y_valid_predict = self.model_architecture(self.X_valid,self.keep_prob,reuse=True)
 
         # Loss on validation
-        self.valid_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=Y_valid_predict, labels=self.Y_valid,name='valid_loss'))
+        self.valid_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=Y_valid_predict, labels=self.Y_valid,name='valid_loss'))
 
     # define train actions per epoch
     def train_epoch(self,sess):
@@ -104,7 +179,7 @@ class CNN(object):
         Xbatch,Ybatch,indx=mf.read_nxt_batch(X,Y,self.batch_size,indx)    # take the right batch
 
         while Xbatch is not None:     # loop through train batches:
-            mean_loss, _ = sess.run([self.train_loss, self.update_ops], feed_dict={self.X_train: Xbatch ,self.Y_train: Ybatch,self.keep_prob:0.3})
+            mean_loss, _ = sess.run([self.train_loss, self.update_ops], feed_dict={self.X_train: Xbatch ,self.Y_train: Ybatch,self.keep_prob:self.dropout})
             Xbatch,Ybatch,indx=mf.read_nxt_batch(X,Y,self.batch_size,indx)
             if math.isnan(mean_loss):
                 print('train cost is NaN')
@@ -201,9 +276,9 @@ class CNN(object):
     def define_predict_operations(self):
         self.keep_prob    = tf.placeholder(dtype=tf.float32,name='keep_prob')       # placeholder for keeping dropout probality
 
-        self.X_eval = tf.placeholder(dtype=tf.float32, shape=(None, self.height,self.width,self.chan),name='X_eval')  # Define this
+        self.X_eval = tf.placeholder(dtype=tf.float32, shape=(None, self.n_input),name='X_eval')  # Define this
 
-        self.Y_eval = tf.placeholder(dtype=tf.int32, shape=(None, ),name='Y_eval')  # Define this
+        self.Y_eval = tf.placeholder(dtype=tf.int32, shape=(None, self.n_classes),name='Y_eval')  # Define this
 
         self.Y_eval_predict =tf.nn.softmax(self.model_architecture(self.X_eval,keep_prob,reuse=True,is_training=False)) # make a prediction using inference softmax
 
