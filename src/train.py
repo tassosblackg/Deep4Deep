f969ba5e088dd9e6bf517d17f8235b05701f3d11
soylisK <soylis.k@gmail.com>
2019-06-03 20:58:37 +0300

    train_approved

--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ from cnn_model import CNN
 from lib.model_io import get_model_id
 from lib.model_io import restore_variables
 from lib.model_io import read_model_id
+from tensorflow.python import debug as tf_debug
 import read_img as rim
 import model_func as mf
 
@@ -20,49 +21,53 @@ try:
     # model_id = read_model_id
     n_tfiles=600 # how many train files will read per step
     n_vfiles=round(0.567*n_tfiles) # number of  validation files to be read per iter
-    # print("a= \n")
-    # print(n_vfiles)
+
     # cheat count files number
     total_inp_files = len(os.listdir(path_to_train_set))
-
 
     # Create the network
     network = CNN(model_id)
     iter=0
     # mf.input(network,n_tfiles,n_vfiles)
     # print(network.Xtrain_in)
-    for i in range(1,total_inp_files,n_tfiles):
-    # loop until all data are read
 
-        mf.input(network,n_tfiles,n_vfiles) # read input data
 
-        with tf.device('/gpu:0'):
+    with tf.device('/gpu:0'):
 
-            # Define the train computation graph
-            network.define_train_operations()
-            saver = tf.train.Saver()
-            # Train the network
-            sess = tf.Session(config=tf.ConfigProto(allow_soft_placement = True)) # session with log about gpu exec
+        # Define the train computation graph
+        network.define_train_operations()
+        # saver = tf.train.Saver()
+        # Train the network
+        sess = tf.Session(config=tf.ConfigProto(allow_soft_placement = True)) # session with log about gpu exec
+        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+
+        # ITerate through input data
+        for i in range(1,total_inp_files,n_tfiles):
+        # loop until all data are read
+
+            mf.input(network,n_tfiles,n_vfiles) # read input data
+            if(iter==0):
+                # initialize train variables
+                init_op = tf.group(tf.global_variables_initializer())
+                sess.run(init_op)
+            else:
+                # tf.reset_default_graph()
+                # new_saver = tf.train.import_meta_graph('saved_models/model.ckpt.meta')
+                # new_saver.restore(sess, tf.train.latest_checkpoint('./saved_models'))
+                restore_variables(sess)
             #sess= tf.Session()
-            try:
-                print(iter)
-                network.train(sess,iter,saver)
-                iter += 1
-                flag = 0
-                if(network.kill):break # if overfitting kill loop ??
-            except KeyboardInterrupt:
-                print()
-                flag = 1
-            finally:
-                flag = 1
-                sess.close()
+
+            print(iter)
+            network.train(sess,iter)
+            iter += 1
+            flag = 0
+            # if(network.kill):break # if overfitting kill loop ??
+        sess.close()
 except KeyboardInterrupt:
     flag=1
 except Exception as e:
     flag=1
-    # template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-    # message = template.format(type(e).__name__, e.args)
-    # print(message)
+
     print("\n")
     traceback.print_exc()
 
@@ -71,6 +76,8 @@ except Exception as e:
 if (flag==1):
     file1 = os.path.basename(path_to_train_set)+"_status.txt"
     if(os.path.exists(file1)): os.remove(file1)
+    if(os.path.exists("dev_info_labels_status.txt")):os.remove("dev_info_labels_status.txt")
     file2 = os.path.basename(path_to_valid_set)+"_status.txt"
     if(os.path.exists(file2)): os.remove(file2)
     if(os.path.exists('labels_status.txt')):os.remove('labels_status.txt')
+    if(os.path.exists("train_info_labels_status.txt")):os.remove("train_info_labels_status.txt")
