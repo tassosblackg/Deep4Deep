ca0e497a457b2fcd0ce0333c1fa07f7ec0b96398
soylisK <soylis.k@gmail.com>
2019-06-13 12:51:46 +0300

    upd..test placeholder

--- src/test_place.py
+++ src/test_place.py
@@ -1,15 +1,17 @@
 import model_func as mf
 import read_img as rim
 import tensorflow as tf
+n_tfiles = 10
 n_input =64*17*1
 n_classes = 2
 x_in,y_in,size = rim.read_Data("ASVspoof2017_V2_train_fbank", "train_info.txt",n_tfiles)
-
+#rim.read_Data("ASVspoof2017_V2_train_dev", "dev_info.txt",n_vfiles)
 X = tf.placeholder(dtype=tf.float32, shape=(None,n_input),name='X_train')
-Y = tf.placeholder(dtype=tf.int32,shape=(None, n_classes), name='Y_train'))
+Y = tf.placeholder(dtype=tf.float32,shape=(None, n_classes), name='Y_train')
 
-out_x = X
+out_x = Y
 
-With tf.Session() as sess:
-    print(sess.run(out_x,feed_dict={X:x_in}))
+out_y = tf.nn.softmax(Y)
+with tf.Session() as sess:
+    print(sess.run(out_y,feed_dict={Y:y_in}))
     # print()
