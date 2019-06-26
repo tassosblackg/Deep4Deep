b16d1e1622f6fc9c2e56ded2af6750efeddff7a1
soylisK <soylis.k@gmail.com>
2019-06-26 05:07:00 +0300

    upd..

--- src/predict.py
+++ src/predict.py
@@ -11,13 +11,14 @@ from tensorflow.python.tools import inspect_checkpoint as chkp
 
 flag=0
 try:
+    LOGDIR_eval = '../graph-eval'
     # path_to_train_set = "/home/tassos/Desktop/DATA_ASR/ASVspoof2017_V2_train_fbank"
     path_to_eval_set = "/home/tassos/Desktop/DATA_ASR/ASVspoof2017_V2_train_eval"
     total_eval_files = len(os.listdir(path_to_eval_set))
     # total_inp_files = len(os.listdir(path_to_train_set))
     model_id = get_model_id()
 
-    n_tfiles=550
+    n_tfiles=1
 
     iter=0
 
@@ -29,10 +30,12 @@ try:
         # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
 
         # Define the train computation graph
-        network.define_graph_op()
+        network.define_predict_operations()
         init_op = tf.group(tf.global_variables_initializer())
         sess.run(init_op)
-
+        if (os.path.exists(LOGDIR_eval)):
+            sh.rmtree(LOGDIR_eval)
+        writer = tf.summary.FileWriter(LOGDIR_train, sess.graph)
 
         # # ITerate through input data --\SUBSET\
 
@@ -46,9 +49,10 @@ try:
 
             print(iter)
             # network.train(sess,writer_train,writer_valid,iter)
+            network.predict_utterance(sess,writer,iter)
             iter += 1
             flag = 0
-
+        network.calc_f1_score()
         sess.close()
 except KeyboardInterrupt :
     flag = 1
