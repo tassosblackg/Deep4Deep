139910e419a13382015059805c4aab5d9d56cca2
soylisK <soylis.k@gmail.com>
2019-07-05 01:11:43 +0300

    check label ohe compare..

--- src/testRead/test_labels.py
+++ src/testRead/test_labels.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+import numpy as np
 import read_img as ri
 
 
@@ -11,3 +14,14 @@ print(sub_labels)
 print('\n')
 ohe = ri.one_hot_encode(sub_labels)
 print(ohe)
+a = np.empty(0)
+a=[[0. ,1.]]
+# b= np.empty(0)
+# b.append([1., 0.])
+print('Tuple\n')
+print(ohe[2,:])
+
+if (np.array_equal(ohe[0,:],[0.,1.])):
+    print('1')
+else:
+    print('0')
